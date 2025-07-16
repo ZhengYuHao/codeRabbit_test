6c381819c1392f61a3bc5ece11176f6cda66e896
xavier <xaviertseng4@gmail.com>
2025-07-16 10:38:10 +0800

    from coderabbit debug

--- simple_utils.py
+++ simple_utils.py
@@ -1,4 +1,9 @@
-def calculate():
-    result = []
-    for item in data:
-        result.append(item * 2
+def double_list_items(data: list[float]) -> list[float]:
+    """
+    接收一个包含数字的列表，将每个元素都乘以2后返回新的列表。
+    参数:
+        data (list[float]): 输入的数字列表。
+    返回:
+        list[float]: 每个元素都乘以2的新列表。
+    """
+    return [item * 2 for item in data]
